22a7679233a871bc5e454670b2823fd92c3bc0f2
D.a.V.o <31695115+didiervega@users.noreply.github.com>
2021-04-22 14:33:54 -0300

    updating MCI to object

--- MultiCentralityIndex.py
+++ MultiCentralityIndex.py
@@ -11,9 +11,11 @@ import networkx as nx
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
+
 cntMap = {'Degree':nx.degree_centrality, 'Eigenvector':nx.eigenvector_centrality_numpy, 
-          'StructuralHoles':nx.constraint, 'Pagerank':nx.pagerank_numpy, 
-          'Betweenness':nx.betweenness_centrality,'Closeness':nx.closeness_centrality}
+          'Pagerank':nx.pagerank_numpy, 'Closeness':nx.closeness_centrality,
+          'StructuralHoles':nx.constraint,'Betweenness':nx.betweenness_centrality}
+
 
 def cntV2DF(cntV):
     return pd.DataFrame(list(cntV.values()))
@@ -22,27 +24,15 @@ def checkNaNs(cntV,cnt):
     x = 1.0 if cnt == 'StructuralHoles' else 0.0
     cntV2 = { d:x  if np.isnan(cntV[d]) else cntV[d] for d in cntV}
     return cntV2
-        
-def calcCentrality(G,cnt):
-    cntV = dict()
-    node_dict = dict(list(dict(G.nodes(data=True)).values())[0])
+ 
+class MCI:
     
-    assert cnt in cntMap.keys(), ('calcCettrality: wrong centrality value or not implemented yet. Available:', list(cntMap.keys()))
-    
-    if node_dict.get(cnt) is None:
-        cntV = cntMap[cnt](G)
-        cntV = checkNaNs(cntV,cnt)        
-        nx.set_node_attributes(G, cntV, cnt)
-        
-    return cntV2DF(dict(G.nodes(data=cnt)))
-
-def getMatrixFeaturesGraph(G,cnts):
-    """
-        Computes the matrix (dataframe) of features for a given single graph in the repository,
-        This function is the equivalent to lines 5 - 11 of the Algorithm 1 from:
-        "A multi-centrality index for graph-based keyword extraction"
-        Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
-        
+    def __init__(self):
+        pass       
+       
+    def calcCentrality(self,G,cnt):
+        """
+        Computes the given centrality cnt in the graph G.
         
         Parameters
         --------
@@ -53,110 +43,195 @@ def getMatrixFeaturesGraph(G,cnts):
         Returns:
         --------
         
-            dataframe: A dataframe in whichs rows are nodes and columns the 
-                corresponding values of each centrality measure, which columns names 
-                the respective centralities.
+            dataframe: A dataframe in which the column corresponds to values of 
+            the centrality measure
             
                 
     """ 
     
-    sc = MinMaxScaler()
-    mtxDoc = pd.DataFrame()
-    mtxDoc['Word'] = list(G.nodes)
-    
-    for cnt in cnts:
-        val = calcCentrality(G,cnt)        
-        #val = cntV2DF(cval)
-        # Normalizing the data
-        val = sc.fit_transform(val)
-        mtxDoc[cnt] = val
-    
-    return mtxDoc
-
-
-def getPC1(mtxFeatures, setCentralities):
-    """
-        Computes the first Principal Component from the table of centrality measures,
-        As explained in Section '4.4. The MCI approach' from:
-        "A multi-centrality index for graph-based keyword extraction"
-        Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
+        cntV = dict()
+        node_dict = dict(list(dict(G.nodes(data=True)).values())[0])
         
+        assert cnt in cntMap.keys(), ('calcCettrality: wrong centrality value or not implemented yet. Available:', list(cntMap.keys()))
         
-        Parameters
-        --------
-                
-            mtxFeatures : A dataframe in whichs rows are nodes and columns the 
-                corresponding values of each centrality measure.
-            setCentralities: The list of centrality measures to be considered.
-                  
-                
-        Returns:
-        --------
-        
-            dataframe : The first principal component, which columns names 
-                are the respective centralities and has only one row, the component values.
+        if node_dict.get(cnt) is None:
+            cntV = cntMap[cnt](G)
+            cntV = checkNaNs(cntV,cnt)        
+            nx.set_node_attributes(G, cntV, cnt)
             
-                
-    """           
-    
-    sc = StandardScaler()
-    A = mtxFeatures.loc[:,setCentralities]
-    # normalize data
-    A = pd.DataFrame(data = sc.fit_transform(A),  columns = list(A))    
-    # create the PCA instance
-    pca = PCA(n_components=1)
-    # fit on data
-    pca.fit(A)
-    # access values and vectors    
-    PC1 = pd.DataFrame(data=pca.components_, columns = list(A))
-    return PC1
-    
+        return cntV2DF(dict(G.nodes(data=cnt)))
 
-def MCI_PC1(G, PC1, N):
-    """
-        Computes the Multi-Centrality index from:
-        Vega-Oliveros, Didier; Gomes, Pedro; Milios, Evangelos; Berton, Lilian.
-        "A multi-centrality index for graph-based keyword extraction"
-        Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
-        The MCI is defined as the 1D principal component from a set of centrality measures.
+
+    def getMatrixFeaturesGraph(self,G,cnts):
+        """
+            Computes the matrix (dataframe) of features for a given single graph in the repository,
+            This function is the equivalent to lines 5 - 11 of the Algorithm 1 from:
+            "A multi-centrality index for graph-based keyword extraction"
+            Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
+            
+            
+            Parameters
+            --------
+                G : The networkx graph.                
+                cnts: The list of centrality measures to be considered.
+                      
+                    
+            Returns:
+            --------
+            
+                dataframe: A dataframe in whichs rows are nodes and columns the 
+                    corresponding values of each centrality measure, which columns names 
+                    the respective centralities.
+                
+                    
+        """ 
         
-        Parameters
-        --------
-                
-            G : The networkx graph.
-            PC1 : A datafram with the first principal component (columns are the component elements),
-                and columns names are the respective centralities.
-            N: The number of best MCI ranked node ids to return (-1 to return all nodes ranked) 
-                  
-                
-        Returns:
-        --------
+        sc = MinMaxScaler()
+        mtxDoc = pd.DataFrame()
+        mtxDoc['Word'] = list(G.nodes)
         
-            dataframe : the N best ranked nodes and scores according to the MCI
+        for cnt in cnts:
+            val = self.calcCentrality(G,cnt) 
+            # Normalizing the data
+            val = sc.fit_transform(val)
+            mtxDoc[cnt] = val
+        
+        return mtxDoc
+    
+    
+    def getPC1(self, mtxFeatures, setCentralities=None):
+        """
+            Computes the first Principal Component from the table of centrality measures,
+            As explained in Section '4.4. The MCI approach' from:
+            "A multi-centrality index for graph-based keyword extraction"
+            Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
+            
+            
+            Parameters
+            --------
+                    
+                mtxFeatures : A dataframe in whichs rows are nodes and columns the 
+                    corresponding values of each centrality measure.
+                setCentralities: The list of centrality measures to be considered.
+                      
+                    
+            Returns:
+            --------
+            
+                dataframe : The first principal component, which columns names 
+                    are the respective centralities and has only one row, the component values.
                 
-    """           
-    
-    sc = MinMaxScaler()
-    #sc = StandardScaler()
-    
-    G_Words = pd.DataFrame()
-    G_Words['Word'] = list(G.nodes)
-    G_Words['MCI'] = np.zeros((len(G.nodes),1))
-    for cnt in list(PC1):
-        val = calcCentrality(G,cnt)
-        # Normalizing the data
-        val = sc.fit_transform(val)
-        G_Words[cnt] = val
-        G_Words['MCI'] += G_Words[cnt]*PC1.loc[0,cnt] 
-    
-    keynodes = G_Words.sort_values(by='MCI', ascending=False).loc[:,['Word','MCI']]
-    
-    if N == -1:
-        return keynodes.reset_index(drop=True)
-    else: 
-        return keynodes.reset_index(drop=True).head(N)
-    
-   
+                    
+        """           
+        
+        sc = StandardScaler()
+        if setCentralities is None:
+            setCentralities =  list(mtxFeatures.select_dtypes(include=['int',"float" ]).columns)
+        A = mtxFeatures.loc[:,setCentralities]
+        # normalize data
+        A = pd.DataFrame(data = sc.fit_transform(A),  columns = list(A))    
+        # create the PCA instance
+        pca = PCA(n_components=1)
+        # fit on data
+        pca.fit(A)
+        # access values and vectors    
+        PC1 = pd.DataFrame(data=pca.components_, columns = list(A))
+        return PC1
+        
+    def getPC1FromGraph(self, G, setCentralities=None):
+        """
+            Returns the first Principal Component from the the give graph by
+            considering the a set of centrality features.
+            
+            "A multi-centrality index for graph-based keyword extraction"
+            Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
+            
+            
+            Parameters
+            --------
+                    
+                G : The networkx graph.
+                setCentralities: The list of centrality measures to be considered.
+                If None, then it considers all the possible centralities.
+                      
+                    
+            Returns:
+            --------
+            
+                dataframe : The first principal component, which columns names 
+                    are the respective centralities and has only one row, the component values.
+                
+                    
+        """    
+        
+        if setCentralities is None:
+            setCentralities = list(cntMap.keys())
+            
+        mtxDoc = self.getMatrixFeaturesGraph(G,setCentralities)
+        PC1 = self.getPC1(mtxDoc, setCentralities)
+        return PC1
+        
+    def getMCI_PCA(self,G, PC1=None, setCetralities=None, N=-1):
+        """
+            Computes the Multi-Centrality index from:
+            Vega-Oliveros, Didier; Gomes, Pedro; Milios, Evangelos; Berton, Lilian.
+            "A multi-centrality index for graph-based keyword extraction"
+            Information Processing & Management. 56. 102063. 10.1016/j.ipm.2019.102063.  
+            The MCI is defined as the 1D principal component from a set of centrality measures.
+            
+            Parameters
+            --------
+                    
+                G : The networkx graph.
+                PC1 : A datafram with the first principal component 
+                    (columns are the component elements and columns names are 
+                    the respective centralities) extracted from a matrix of 
+                    features of graphs.
+                    
+                    If PC1 = None, then the PC1 is calculated from the given graph G,
+                    considering the set of Centralities
+                
+                setCentralities: (Optional) the list of centrality measures to be considered
+                    in the case that none PC1 value is passed.
+                
+                N: The number of best MCI ranked node ids to return 
+                    (-1 to return all nodes ranked) 
+                      
+                    
+            Returns:
+            --------
+            
+                dataframe : the N best ranked nodes and scores according to the MCI
+                    
+        """           
+        
+        assert PC1 is not None or setCetralities is not None, ('getMCI_PCA: Please provide either a PC1 or setCentralities parameter. Available centralities are:', list(cntMap.keys()))
+        sc = MinMaxScaler()
+        #sc = StandardScaler()
+        
+        G_Words = pd.DataFrame()
+        G_Words['Word'] = list(G.nodes)
+        G_Words['MCI'] = np.zeros((len(G.nodes),1))
+        
+        if PC1 is None:
+            PC1 = self.getPC1FromGraph(G,setCetralities)
+                
+        for cnt in list(PC1):
+            val = self.calcCentrality(G,cnt)
+            
+            # Normalizing the data
+            val = sc.fit_transform(val)
+            G_Words[cnt] = val
+            G_Words['MCI'] += G_Words[cnt]*PC1.loc[0,cnt] 
+        
+        keynodes = G_Words.sort_values(by='MCI', ascending=False).loc[:,['Word','MCI']]
+        
+        if N == -1:
+            return keynodes.reset_index(drop=True)
+        else: 
+            return keynodes.reset_index(drop=True).head(N)
+       
+  
  
 if __name__ == "__main__":       
 
@@ -173,12 +248,17 @@ if __name__ == "__main__":
     G=nx.read_edgelist("edgelist_art_and_culture-20914080.txt")
     nx.draw_networkx(G)
         
-    #Number of requested nodes
+    #Number of requested nodes -1, wich means all the nodes
     N = -1
     
-    PC1 = getPC1(mtxFeatures, setCentralities)
-    keywords = MCI_PC1(G, PC1, N)
+    mc = MCI()
+    
+    PC1 = mc.getPC1(mtxFeatures, setCentralities)
+    keywords = mc.getMCI_PCA(G, PC1, N)[:15]
     print(keywords)
+    print(" ... ")
+    #display(keywords)
+    
     
 
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## How to use it and examples 
 
-This is a notebook showing how to use the code of the proposed method for multicentrality index, which was employed for the analysis in [1]. The code is in Python3, and some toolboxes are necessary to run the commands. 
+This is a notebook showing how to use the code of the proposed method for Multi-Centrality Index, which was employed for the analysis of keywords in [1]. The code is in Python3, and some toolboxes are necessary to run the commands. 
 
 The following very common packages are necessary for running the code:
 
@@ -19,9 +19,9 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 ```
 
-Then, you can run the example for a preloaded matrix of features (centralities) from a previous constructed graph-of-words (network) with the co-occurrence approach. 
+Then, you can run the example from a preloaded matrix of features (centralities) of a previous constructed graph-of-words (network) with the co-occurrence approach. 
 
-The result is each word of the graph sorted by the corresponding Multi-centrality Index (**MCI**) value. 
+Each word of the graph is sorted by the corresponding Multi-centrality Index (**MCI**) value. 
 In this example, the MCI is the combination of these centrality measures: [*'Degree','Pagerank','Eigenvector','StructuralHoles'*]
 
 Top words are considered the keywords of the text.
@@ -32,38 +32,40 @@ You run the code as follow:
 python MultiCentralityIndex.py
 ```
 
-
-                Word       MCI
-    0          MAMET  1.501088
-    1           PLAY  1.484412
-    2       DIRECTOR  0.968968
-    3      ANARCHIST  0.887786
-    4        THEATER  0.712872
-    5       PULITZER  0.647991
-    6         LONDON  0.635058
-    7          GOOLD  0.619282
-    8           YEAR  0.605723
-    9      GLENGARRY  0.572361
-    10         DEBUT  0.530749
-    11         DAVID  0.311259
-    12          YORK  0.225655
-    13   PRIZEWINNER  0.215347
-    14         PRIZE  0.208866
-    15        RUPERT  
-
-
-<b>              ...              </b>
+               Word       MCI
+    0         MAMET  1.501088
+    1          PLAY  1.484412
+    2      DIRECTOR  0.968968
+    3     ANARCHIST  0.887786
+    4       THEATER  0.712872
+    5      PULITZER  0.647991
+    6        LONDON  0.635058
+    7         GOOLD  0.619282
+    8          YEAR  0.605723
+    9     GLENGARRY  0.572361
+    10        DEBUT  0.530749
+    11        DAVID  0.311259
+    12         YORK  0.225655
+    13  PRIZEWINNER  0.215347
+    14        PRIZE  0.208866
+     ... 
 
 
 ![png](output_8_0.png)
+    
+
+
 
 
 Besides, you can import and use the code as your necessity. For example, lets calculate the MCI for the <a href='http://www-personal.umich.edu/~mejn/centrality/' target='_blank'>Coauthorships in network science</a> <table><tr><td><span><b> A figure depicting the largest component of this network</b></span> Extracted from Prof. Newman <a href='http://www-personal.umich.edu/~mejn/netdata' target='_blank'> Web site</a></td><td><img src='http://www-personal.umich.edu/~mejn/centrality/labeleds.png'></td></tr></table>
 
 
 ``` python
-import MultiCentralityIndex as mc
+import MultiCentralityIndex as MCI
 import networkx as nx
+
+# Creating the MCI object
+mc = MCI.MCI()
 
 #loading the netscience graph
 G = nx.read_gml('netscience.gml',label='label')
@@ -73,56 +75,42 @@ node_size=[float(G.degree(v)) for v in G]
 nx.draw_networkx(G, arrows=True, node_size=20, node_color=node_size,edge_color='grey',alpha=.5,with_labels=False)
 ```
 
+
+
+
 ![png](output_11_0.png)
+    
 
 
 Now, let's define the set of centrality measures to be calculated as
 ``` python 
 setCentralities = ['Degree','Pagerank','Eigenvector','StructuralHoles','Closeness', 'Betweenness']
 ```
-In this example we'll calculate the MCI for a single graph (network). First, we call the ```getMatrixFeaturesGraph``` function getting the matrix of centrality measures of the graph (``mtxDoc``). Then, we call the ```getPC1``` function for computing the first Principal Component (PC1) of the graph.
-
-**Note**: In the case of ref[1], we calculated the matrix of features from the set of graphs in a repository and, then, we computed the first Principal Component (```getPC1``` function) from this matrix of features of the entire repository.
-
-
-
-``` python
-mtxDoc = mc.getMatrixFeaturesGraph(G,setCentralities)
-print(mtxDoc)
-```
-
-
-                     Word    Degree  Pagerank   Eigenvector  StructuralHoles  \
-    0         ABRAMSON, G  0.058824  0.139844  4.162192e-16         0.887188   
-    1         KUPERMAN, M  0.088235  0.220900  4.325825e-16         0.508935   
-    2          ACEBRON, J  0.117647  0.142576  4.613181e-16         0.656586   
-    3          BONILLA, L  0.117647  0.142576  4.617155e-16         0.656586   
-    4     PEREZVICENTE, C  0.117647  0.142576  4.623615e-16         0.6565
-
-
-<b>              ...              </b>
+In this example we are calculating the MCI for a single graph (network). For this, we just call the ```getMCI_PCA``` method. 
 
 
 ```python
-PC1 = mc.getPC1(mtxDoc, setCentralities)
-print(PC1)
+mc.getMCI_PCA(G,setCetralities=setCentralities)[:10]
 ```
 
-         Degree  Pagerank  Eigenvector  StructuralHoles  Closeness  Betweenness
-    0  0.535797  0.434088     0.202644        -0.494563   0.329897     0.360556
-    
 
-Now, we can calculate the MCI for the graph calling the function 
-
-``` python 
-N = 10
-MCI = mc.MCI_PC1(G, PC1, N)
-print(MCI)
-```
-where N means the top N nodes. If N = -1 it returns all the nodes.
 
 
 <div>
+<style scoped>
+    .dataframe tbody tr th:only-of-type {
+        vertical-align: middle;
+    }
+
+    .dataframe tbody tr th {
+        vertical-align: top;
+    }
+    
+    .dataframe thead th {
+        text-align: right;
+    }
+</style>
+
 <table border="1" class="dataframe">
   <thead>
     <tr style="text-align: right;">
@@ -185,6 +173,290 @@ where N means the top N nodes. If N = -1 it returns all the nodes.
   </tbody>
 </table>
 </div>
+
+
+
+**Note**: In the case of ref[1], we calculated the matrix of features from a collection or set of graphs-of-words of a repository and, then, we computed the first Principal Component (```getPC1``` method) from this matrix of features of the entire repository
+
+Behind the scene, the ```getMCI_PCA``` is calling the ```getPC1FromGraph``` method, which obtains the matrix of centrality measures of the graph (``mtxDoc``) and calls the ```getPC1``` function for computing the first Principal Component (PC1) of the graph.
+
+For illustration purpose, this is the matrix of features of the graph:
+
+
+```python
+mtxDoc = mc.getMatrixFeaturesGraph(G,setCentralities)
+display(mtxDoc)
+```
+
+
+<div>
+<style scoped>
+    .dataframe tbody tr th:only-of-type {
+        vertical-align: middle;
+    }
+
+    .dataframe tbody tr th {
+        vertical-align: top;
+    }
+    
+    .dataframe thead th {
+        text-align: right;
+    }
+</style>
+<table border="1" class="dataframe">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th>Word</th>
+      <th>Degree</th>
+      <th>Pagerank</th>
+      <th>Eigenvector</th>
+      <th>StructuralHoles</th>
+      <th>Closeness</th>
+      <th>Betweenness</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>0</th>
+      <td>ABRAMSON, G</td>
+      <td>0.058824</td>
+      <td>0.139844</td>
+      <td>1.418503e-15</td>
+      <td>0.887188</td>
+      <td>0.023195</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>1</th>
+      <td>KUPERMAN, M</td>
+      <td>0.088235</td>
+      <td>0.220900</td>
+      <td>1.410715e-15</td>
+      <td>0.508935</td>
+      <td>0.030927</td>
+      <td>0.000071</td>
+    </tr>
+    <tr>
+      <th>2</th>
+      <td>ACEBRON, J</td>
+      <td>0.117647</td>
+      <td>0.142576</td>
+      <td>1.394125e-15</td>
+      <td>0.656586</td>
+      <td>0.041236</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>3</th>
+      <td>BONILLA, L</td>
+      <td>0.117647</td>
+      <td>0.142576</td>
+      <td>1.396850e-15</td>
+      <td>0.656586</td>
+      <td>0.041236</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>4</th>
+      <td>PEREZVICENTE, C</td>
+      <td>0.117647</td>
+      <td>0.142576</td>
+      <td>1.419869e-15</td>
+      <td>0.656586</td>
+      <td>0.041236</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>...</th>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+    </tr>
+    <tr>
+      <th>1584</th>
+      <td>MONDRAGON, R</td>
+      <td>0.029412</td>
+      <td>0.142576</td>
+      <td>1.403821e-15</td>
+      <td>0.880552</td>
+      <td>0.010309</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>1585</th>
+      <td>ZHU, H</td>
+      <td>0.058824</td>
+      <td>0.219644</td>
+      <td>1.405278e-15</td>
+      <td>0.402758</td>
+      <td>0.020618</td>
+      <td>0.000035</td>
+    </tr>
+    <tr>
+      <th>1586</th>
+      <td>HUANG, Z</td>
+      <td>0.029412</td>
+      <td>0.104042</td>
+      <td>1.407069e-15</td>
+      <td>0.880552</td>
+      <td>0.013745</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>1587</th>
+      <td>ZHU, J</td>
+      <td>0.029412</td>
+      <td>0.104042</td>
+      <td>1.409720e-15</td>
+      <td>0.880552</td>
+      <td>0.013745</td>
+      <td>0.000000</td>
+    </tr>
+    <tr>
+      <th>1588</th>
+      <td>ZIMMERMANN, M</td>
+      <td>0.058824</td>
+      <td>0.073159</td>
+      <td>1.422667e-15</td>
+      <td>0.552483</td>
+      <td>0.138159</td>
+      <td>0.000000</td>
+    </tr>
+  </tbody>
+</table>
+<p>1589 rows × 7 columns</p>
+</div>
+
+
+And this is the first Principal Component (PC1) of the graph according to all columns (centralities) in the matrix of features
+
+
+```python
+PC1 = mc.getPC1(mtxDoc)
+print(PC1)
+```
+
+         Degree  Pagerank  Eigenvector  StructuralHoles  Closeness  Betweenness
+    0  0.535797  0.434088     0.202644        -0.494563   0.329897     0.360556
+
+
+Or, you can filter selecting specific centralities
+
+
+```python
+PC1 = mc.getPC1(mtxDoc,setCentralities=['Degree', 'Pagerank', 'StructuralHoles'])
+print(PC1)
+```
+
+         Degree  Pagerank  StructuralHoles
+    0  0.621633  0.532563        -0.574412
+
+
+Now, we can calculate the MCI of the graph considering the previous PC1 and calling the function 
+
+``` python 
+N = 10
+centralNodes = mc.getMCI_PCA(G, PC1, N=N)
+```
+
+where N means the top N nodes. If **N = -1** it returns all the nodes.
+
+
+```python
+N = 10
+MCI = mc.getMCI_PCA(G, PC1, N=N)
+display(MCI)
+```
+
+
+<div>
+<style scoped>
+    .dataframe tbody tr th:only-of-type {
+        vertical-align: middle;
+    }
+
+    .dataframe tbody tr th {
+        vertical-align: top;
+    }
+    
+    .dataframe thead th {
+        text-align: right;
+    }
+</style>
+<table border="1" class="dataframe">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th>Word</th>
+      <th>MCI</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>0</th>
+      <td>BARABASI, A</td>
+      <td>1.091113</td>
+    </tr>
+    <tr>
+      <th>1</th>
+      <td>NEWMAN, M</td>
+      <td>1.026212</td>
+    </tr>
+    <tr>
+      <th>2</th>
+      <td>JEONG, H</td>
+      <td>0.829966</td>
+    </tr>
+    <tr>
+      <th>3</th>
+      <td>YOUNG, M</td>
+      <td>0.623524</td>
+    </tr>
+    <tr>
+      <th>4</th>
+      <td>OLTVAI, Z</td>
+      <td>0.603774</td>
+    </tr>
+    <tr>
+      <th>5</th>
+      <td>BOCCALETTI, S</td>
+      <td>0.601977</td>
+    </tr>
+    <tr>
+      <th>6</th>
+      <td>SOLE, R</td>
+      <td>0.567841</td>
+    </tr>
+    <tr>
+      <th>7</th>
+      <td>KURTHS, J</td>
+      <td>0.519397</td>
+    </tr>
+    <tr>
+      <th>8</th>
+      <td>ALON, U</td>
+      <td>0.510426</td>
+    </tr>
+    <tr>
+      <th>9</th>
+      <td>PASTORSATORRAS, R</td>
+      <td>0.492352</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+
+Clearly, the ranking changes depending on the selected centrality measures. 
+This is why our proposal in Ref[1] of finding the best subset of centralities according to your supervised problem. For instance, applying some Feature Selection methods, correlation analysis, etc.
+
+In unsupervised problems, a good approach could be to select the group of centrality measures less correlated.
+
 
 
 # References
